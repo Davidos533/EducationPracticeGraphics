5c479f0ba00951980b7a066b870a4cd5dba62bfc
Davidos533 <medvedkov29@gmail.com>
2021-11-30 23:14:01 +0300

    made fifth exercise

---
+++ exercise_5_graphics/exercise_5_graphics_task_1/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace exercise_5_graphics_task_1
+{
+    public partial class MainWindow : Window
+    {
+        Label outData;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            outData = new Label();
+
+            outData.HorizontalAlignment = HorizontalAlignment.Left;
+            outData.VerticalAlignment = VerticalAlignment.Top;
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            double startValue;
+            double stopValue;
+            double stepValue;
+
+            StringBuilder outStr = new StringBuilder();
+            panel.Children.Clear();
+            if (double.TryParse(startValueTextBox.Text, out startValue) && double.TryParse(stopValueTextBox.Text, out stopValue) && double.TryParse(stepValueTextBox.Text, out stepValue))
+            {
+                double buf;
+                for (double i = startValue; i < stopValue; i += stepValue)
+                {
+                    try
+                    {
+                        buf = Func(i);
+                        outStr.Append($"\nx={i:0.###}, y={buf:0.###}");
+                    }
+                    catch
+                    {
+                        outStr.Append($"\nno roots");
+                    }
+                }
+
+                outData.Content = outStr.ToString();
+            }
+            else
+            {
+                outData.Content = "Incorrect input";    
+            }
+
+            panel.Children.Add(outData);
+
+        }
+        static double Func(double x)
+        {
+            if (x - 2 <= 0)
+            {
+                throw new Exception();
+            }
+            else
+            {
+                return Math.Log(x - 2);
+            }
+        }
+    }
+}
