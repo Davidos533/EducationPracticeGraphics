ba84ab6ec3c0f01b5f3c11cce2bedb99c2aa78a9
Davidos533 <medvedkov29@gmail.com>
2021-11-30 21:28:21 +0300

    made fourth exercise

---
+++ exercise_4_graphics/exercise_4_graphics_task_1/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows;
+
+namespace exercise_4_graphics_task_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int valueX;
+            int valueN;
+
+            if (int.TryParse(inputN.Text, out valueN) && int.TryParse(inputX.Text, out valueX)&&valueN>0)
+            {
+                answer.Content = $"{foo(valueX, valueN):0.###} ";
+            }
+            else
+            {
+                answer.Content = "Incorrect input";
+            }
+        }
+        private double foo(double x, int n, int counter = 1)
+        {
+            if (counter != n)
+            {
+                return x / (counter + foo(x, n, counter + 1));
+            }
+            else
+            {
+                return counter + x;
+            }
+        }
+    }
+}
---
+++ exercise_4_graphics/exercise_4_graphics_task_2/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Windows;
+
+namespace exercise_4_graphics_task_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int binaryValue;
+        int summ;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void TextBox_OnInput(object sender ,RoutedEventArgs e)
+        {
+            answer.Content = String.Empty;
+
+            summ = 0;
+
+            if (int.TryParse(binaryInput.Text, out binaryValue)&&ChekValToBinary(binaryInput.Text))
+            {
+                foo(binaryValue, ref summ);
+            }
+            else
+            {
+                answer.Content = "Incorrect input";
+            }
+        }
+        private void foo(int binaryNumber, ref int summ, int iter = 0)
+        {
+            if (binaryNumber / 10 != 0 || binaryNumber == 1)
+            {
+                summ += binaryNumber % 10 * (int)Math.Pow(2, iter);
+                foo(binaryNumber / 10, ref summ, iter + 1);
+            }
+            if (iter == 0)
+            {
+                answer.Content = summ.ToString();
+            }
+        }
+        private bool ChekValToBinary(string value)
+        {
+            for (int i = 0; i < value.Length; i++)
+            {
+                if ((value[i] != '0') && (value[i] != '1'))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
