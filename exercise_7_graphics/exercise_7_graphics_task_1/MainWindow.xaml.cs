4a6bbbf7e3ed70626529d2a15b9636f8948df6f3
Davidos533 <medvedkov29@gmail.com>
2021-12-01 22:44:51 +0300

    made seventh exercise

---
+++ exercise_7_graphics/exercise_7_graphics_task_1/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace exercise_7_graphics_task_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void inputString_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            ProcessStrings();
+        }
+        private void ProcessStrings()
+        {
+            if (inputString.Text != String.Empty && editableString.Text != String.Empty && newString.Text != String.Empty)
+            {
+                StringBuilder inputSb= new StringBuilder(inputString.Text);
+
+                resultOut.Content=inputSb.Replace(editableString.Text,newString.Text).ToString();
+            }
+            else
+            {
+                resultOut.Content = "Please input all values";
+            }
+        }
+    }
+}
---
+++ exercise_7_graphics/exercise_7_graphics_task_2/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace exercise_7_graphics_task_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void inputString_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (inputString.Text != String.Empty)
+            {
+                string[] words = inputString.Text.Split(' ');
+
+                int longestWordId = 0;
+
+                for (int i = 0; i < words.Length; i++)
+                {
+                    if (words[longestWordId].Length < words[i].Length)
+                    {
+                        longestWordId = i;
+                    }
+                }
+                outData.Content = words[longestWordId];
+            }
+            else
+            {
+                outData.Content = "Enter something";
+            }
+        }
+    }
+}
