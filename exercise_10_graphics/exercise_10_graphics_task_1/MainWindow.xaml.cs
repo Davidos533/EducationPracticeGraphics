5ac627b91d11d34c949323a34690a2da8b9b2edb
Davidos533 <medvedkov29@gmail.com>
2021-12-03 16:56:57 +0300

    made tenth exercise

---
+++ exercise_10_graphics/exercise_10_graphics_task_1/MainWindow.xaml.cs
@@ -0,0 +1,132 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace exercise_10_graphics_task_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ProgramFileDirectoryManager fileManager;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            fileManager = new ProgramFileDirectoryManager();
+            UpdateDirectoryElements();
+        }
+        // метод для обновления данных текущей дериктории
+        private void UpdateDirectoryElements()
+        {
+            currentDirectory.Content = fileManager.m_currentManagerDirectory;       // установка значения текущей дериктории
+            dataPanel.Children.Clear(); // очитска списка элементов панели
+            statusCode.Content= DecryptStatusCode(fileManager.ShowAllCurrentDirectoryFD(dataPanel));       // получение новго списка
+            UpdateLabelsEventers();     // обновление обработчиков событий
+            
+        }
+        // метод для перехода на директорию выше
+        private void buttonGoUpDirectory_Click(object sender, RoutedEventArgs e)
+        {
+            statusCode.Content=DecryptStatusCode(fileManager.GoBackDirectory());
+            UpdateDirectoryElements();
+        }
+        private void buttonMakeDirectory_Click(object sender, RoutedEventArgs e)
+        {
+            statusCode.Content=DecryptStatusCode(fileManager.CreateDirectory(textBoxMakeDirectory.Text));
+            UpdateDirectoryElements();
+        }
+        private void buttonMakeFile_Click(object sender, RoutedEventArgs e)
+        {
+            statusCode.Content = DecryptStatusCode(fileManager.CreateFile(textBoxMakeFileFileName.Text,textBoxMakeFileFileData.Text));
+            UpdateDirectoryElements();
+        }
+        private void buttonConcatanateFiles_Click(object sender, RoutedEventArgs e)
+        {
+            statusCode.Content = DecryptStatusCode(fileManager.AppendTextToFile(textBoxConcatanateEditableFileName.Text, textBoxConcatanateAppendableFileName.Text));
+            UpdateDirectoryElements();
+        }
+        private void buttonRemoveElemnt_Click(object sender, RoutedEventArgs e)
+        {
+            statusCode.Content = DecryptStatusCode(fileManager.DeleteFileOrDirectory(currentSelectedElement.Content.ToString().Substring(0, currentSelectedElement.Content.ToString().Length - 1)));
+            UpdateDirectoryElements();
+        }
+        // метод для обновления обработчиков событий
+        private void UpdateLabelsEventers()
+        {
+            for (int i = 0; i < dataPanel.Children.Count; i++)
+            {
+                dataPanel.Children[i].MouseLeftButtonDown += (sender, e) =>
+                    {
+                        ResetPanelElementsColors();
+                        ((Label)sender).Background = Brushes.Yellow;
+                        currentSelectedElement.Content = ((Label)sender).Content.ToString();
+
+                        if (e.ClickCount == 2)
+                        {
+                            string elementName = currentSelectedElement.Content.ToString().Substring(0, currentSelectedElement.Content.ToString().Length - 1);
+                            StringBuilder data = new StringBuilder();
+                            if ((fileManager.GoToDirectory(elementName)) == ProgramFileDirectoryManager.StatusCode.SuccessfullyStepped)
+                            {
+                                ResetPanelElementsColors();
+                                UpdateDirectoryElements();
+                            }
+                            else if (fileManager.ReadFile(elementName, ref data)==ProgramFileDirectoryManager.StatusCode.SuccessfullyRead)
+                            {
+                                fileData.Content = data.ToString();
+                                fileDataPanel.Height = fileData.Height;
+                                data.Clear();
+                            }
+                        }
+                    };
+            }
+        }
+        private void ResetPanelElementsColors()
+        {
+            for (int i = 0; i < dataPanel.Children.Count; i++)
+            {
+                ((Label)dataPanel.Children[i]).Background = Brushes.LightYellow;
+            }
+        }
+        static string GetStringFormStringArrayRange(string[] array, int startIndex, int stopIndex)
+        {
+            StringBuilder returnableData = new StringBuilder();
+
+            try
+            {
+                for (int i = startIndex; i < stopIndex + 1; i++)
+                {
+                    returnableData.Append($"{array[i]} ");
+                }
+                return returnableData.ToString();
+            }
+            catch
+            {
+                return "";
+            }
+
+        }
+        static string DecryptStatusCode(ProgramFileDirectoryManager.StatusCode currentSelectedElement)
+        {
+            switch (currentSelectedElement)
+            {
+                case ProgramFileDirectoryManager.StatusCode.AlreadyCreated: return "already created";
+                case ProgramFileDirectoryManager.StatusCode.DirectoryNotFound: return "directroty not found";
+                case ProgramFileDirectoryManager.StatusCode.FileNotFound: return "file not found";
+                case ProgramFileDirectoryManager.StatusCode.SomeError: return "some error";
+                case ProgramFileDirectoryManager.StatusCode.SuccessfullyAppended: return "successfully appended";
+                case ProgramFileDirectoryManager.StatusCode.SuccessfullyCreated: return "successfully created";
+                case ProgramFileDirectoryManager.StatusCode.SuccessfullyStepped: return "successfully stepped";
+                case ProgramFileDirectoryManager.StatusCode.SuccessfullyGet: return "successfully get";
+                case ProgramFileDirectoryManager.StatusCode.SuccessfullyRead: return "successfully read";
+                case ProgramFileDirectoryManager.StatusCode.SuccessfullyDelete: return "successfully delete";
+                case ProgramFileDirectoryManager.StatusCode.ElementNotFound: return "element not found";
+                default: return "unknown code";
+            }
+        }
+
+
+    }
+}
---
+++ exercise_10_graphics/exercise_10_graphics_task_1/ProgramFileDirectoryManager.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace exercise_10_graphics_task_1
+{
+    class ProgramFileDirectoryManager
+    {
+        public enum StatusCode
+        {
+            SuccessfullyCreated,
+            AlreadyCreated,
+            SuccessfullyAppended,
+            SuccessfullyStepped,
+            SuccessfullyGet,
+            SuccessfullyRead,
+            SuccessfullyDelete,
+            SomeError,
+            DirectoryNotFound,
+            FileNotFound,
+            ElementNotFound,
+        }
+        public string m_currentManagerDirectory { get; private set; }
+
+        public ProgramFileDirectoryManager()
+        {
+            m_currentManagerDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            //m_currentManagerDirectory = m_currentManagerDirectory.Replace("\\","/");
+        }
+        public StatusCode DeleteFileOrDirectory(string name)
+        {
+            try
+            {
+                if (File.Exists($"{m_currentManagerDirectory}\\{name}"))
+                {
+                    File.Delete($"{m_currentManagerDirectory}\\{name}");
+                    return StatusCode.SuccessfullyDelete;
+                }
+                else if (Directory.Exists($"{m_currentManagerDirectory}\\{name}"))
+                {
+                    Directory.Delete($"{m_currentManagerDirectory}\\{name}",true);
+                    return StatusCode.SuccessfullyDelete;
+                }
+                else
+                {
+                    return StatusCode.ElementNotFound;
+                }
+            }
+            catch (Exception e)
+            {
+                return StatusCode.SomeError;
+            }
+        }
+        public StatusCode ReadFile(string filename,ref StringBuilder fileData)
+        {
+            try
+            {
+                if (!File.Exists($"{m_currentManagerDirectory}\\{filename}"))
+                {
+                    return StatusCode.FileNotFound;
+                }
+                else
+                {
+                    fileData=new StringBuilder(File.ReadAllText($"{m_currentManagerDirectory}\\{filename}"));
+                    return StatusCode.SuccessfullyRead;
+                }
+            }
+            catch
+            {
+                return StatusCode.SomeError;
+            }
+        }
+        public StatusCode ShowAllCurrentDirectoryFD(StackPanel dataPanel)
+        {
+            try
+            {
+                if(m_currentManagerDirectory.Length<3)
+                {
+                    m_currentManagerDirectory += "\\";
+                }
+                foreach (var systemEntry in Directory.GetFileSystemEntries(m_currentManagerDirectory))
+                {
+                    int finalDirectoryPathLength = systemEntry.LastIndexOf('\\');
+                    dataPanel.Children.Add(new Label {Margin=new Thickness(10,10,0,0),FontFamily=new FontFamily("Consolas"),Content=($"{systemEntry.Substring(finalDirectoryPathLength + 1, systemEntry.Length - finalDirectoryPathLength - 1)}\n"),Width=dataPanel.Width});
+                }
+                return StatusCode.SuccessfullyGet;
+            }
+            catch(Exception ex)
+            {
+                return StatusCode.SomeError;
+            }
+        }
+        public StatusCode GoBackDirectory()
+        {
+            try
+            {
+                int finalDirectoryPathLength = m_currentManagerDirectory.LastIndexOf('\\');
+                if (!Directory.Exists(m_currentManagerDirectory.Substring(0, finalDirectoryPathLength)))
+                {
+                    return StatusCode.DirectoryNotFound;
+                }
+                else
+                {
+                    m_currentManagerDirectory = m_currentManagerDirectory.Substring(0, finalDirectoryPathLength);
+                    return StatusCode.SuccessfullyStepped;
+                }
+            }
+            catch 
+            {
+                return StatusCode.SomeError;
+            }
+        }
+        public StatusCode GoToDirectory(string directory)
+        {
+            try
+            {
+                // если указанна дерикория отсутствует
+                if (!Directory.Exists($"{m_currentManagerDirectory}\\{directory}"))
+                {
+                    return StatusCode.DirectoryNotFound;
+                }
+                else
+                {
+                    if (m_currentManagerDirectory.Length == 3)
+                    {
+                        m_currentManagerDirectory = m_currentManagerDirectory.Substring(0, 2);
+                    }
+                    m_currentManagerDirectory = $"{m_currentManagerDirectory}\\{directory}";
+                    return StatusCode.SuccessfullyStepped;
+                }
+            }
+            catch
+            {
+                return StatusCode.SomeError;
+            }
+
+        }
+        public StatusCode AppendTextToFile(string editableFile, string appendFile)
+        {
+            try
+            {
+                string readenData=File.ReadAllText($"{m_currentManagerDirectory}\\{appendFile}");
+
+                File.AppendAllText($"{m_currentManagerDirectory}\\{editableFile}",readenData);
+
+                return StatusCode.SuccessfullyAppended;
+            }
+            catch
+            {
+                return StatusCode.SomeError;
+            }
+        }
+        // метод для слздания файла
+        public StatusCode CreateFile(string fileName,string data)
+        {
+            try
+            {
+                // если файл отсутствует
+                if(!File.Exists($"{m_currentManagerDirectory}\\{fileName}"))
+                {
+                    File.Create($"{m_currentManagerDirectory}\\{fileName}").Close();    // создание файла
+                    if (data != String.Empty)
+                    { 
+                        File.AppendAllText($"{m_currentManagerDirectory}\\{fileName}", data);
+                    }
+                    return StatusCode.SuccessfullyCreated;
+                }
+                else
+                { 
+                    return StatusCode.AlreadyCreated;
+                }
+            }
+            catch(Exception ex)
+            {
+                return StatusCode.SomeError;
+            }
+        }
+        // метод для создание дирекории с заданным именем в папке текущей сборки
+        public StatusCode CreateDirectory(string directoryName)
+        {
+            try
+            {
+                // если дикректория отсутствует
+                if (!Directory.Exists($"{m_currentManagerDirectory}\\{directoryName}"))
+                {
+                    Directory.CreateDirectory($"{m_currentManagerDirectory}\\{directoryName}");   // создание директории
+                    return StatusCode.SuccessfullyCreated;
+                }
+                // иначе если директория уже существует
+                else
+                {
+                    return StatusCode.AlreadyCreated;
+                }
+            }
+            catch
+            {
+                return StatusCode.SomeError;
+            }
+
+        }
+    }
+}
