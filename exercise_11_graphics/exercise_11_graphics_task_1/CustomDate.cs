6e51b244eb127f2d3870c2d5f779c8a206daa62a
Davidos533 <medvedkov29@gmail.com>
2021-12-03 20:11:17 +0300

    made eleventh exercise

---
+++ exercise_11_graphics/exercise_11_graphics_task_1/CustomDate.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace exercise_11_graphics_task_1
+{
+    class CustomDate
+    {
+        public DateTime m_dateTime { get; set; }
+        public bool IsLeap
+        {
+            get
+            {
+                return DateTime.IsLeapYear(m_dateTime.Year);
+            }
+        }
+        // конструктор с параметром
+        public CustomDate(DateTime date)
+        {
+            m_dateTime = date;
+        }
+        // конструктор по умлочнаию
+        public CustomDate()
+        {
+            m_dateTime = new DateTime(2009, 1, 1);
+        }
+        // метод для получения даты предыдущего дня
+        public DateTime GetPreviousDayDate()
+        {
+            return m_dateTime.AddDays(-1);
+        }
+        // метод для получения даты следующего дня
+        public DateTime GetNextDayDate()
+        {
+            return m_dateTime.AddDays(1);
+        }
+        // метод для получения количества дней до конца месяца
+        public int CountDaysToEndOfMonth()
+        {
+            return DateTime.DaysInMonth(m_dateTime.Year, m_dateTime.Month) - m_dateTime.Day;
+        }
+
+    }
+}
---
+++ exercise_11_graphics/exercise_11_graphics_task_1/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace exercise_11_graphics_task_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        CustomDate customDate;
+        public MainWindow()
+        {
+            InitializeComponent();
+            customDate = new CustomDate();
+            dateInput.Text = customDate.m_dateTime.ToString().Substring(0, 10);
+        }
+
+        private void dateInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                customDate.m_dateTime = DateTime.Parse(dateInput.Text);
+                nextDay.Content=$"Next day: {customDate.GetNextDayDate().ToString().Substring(0,10)}";
+                previousDay.Content=$"Previous day: {customDate.GetPreviousDayDate().ToString().Substring(0, 10)}";
+                thisYearIsLeap.Content=$"This year {(customDate.IsLeap?"is":"not")} leap";
+                daysForEndOfMonth.Content = $"Days for end of mont: {customDate.CountDaysToEndOfMonth()}";
+            }
+            catch
+            {
+                daysForEndOfMonth.Content = "waiting input";
+                thisYearIsLeap.Content = "waiting input";
+                nextDay.Content = "waiting input";
+                previousDay.Content = "waiting input";
+            }
+        }
+    }
+}
