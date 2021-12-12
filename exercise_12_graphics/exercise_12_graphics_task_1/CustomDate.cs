90defefc79581ad4b999c6ddf13d58f9b2b6d603
Davidos533 <medvedkov29@gmail.com>
2021-12-12 20:19:12 +0300

    made twelth exercise

---
+++ exercise_12_graphics/exercise_12_graphics_task_1/CustomDate.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace exercise_12_graphics_task_1
+{
+    class CustomDate
+    {
+        public DateTime m_dateTime { get; set; }
+        public bool IsLeap
+        {
+            get
+            {
+                return DateTime.IsLeapYear(m_dateTime.Year);
+            }
+        }
+        // объявление индексатора
+        public DateTime this[int index]
+        {
+            get
+            {
+                return m_dateTime.AddDays(index);
+            }
+
+        }
+        // конструктор с параметром
+        public CustomDate(DateTime date)
+        {
+            m_dateTime = date;
+        }
+        // конструктор по умлочнаию
+        public CustomDate()
+        {
+            m_dateTime = new DateTime(2009, 1, 1);
+        }
+
+        // перегрузка унарного оператора
+        public static bool operator !(CustomDate date)
+        {
+            return date.m_dateTime.Day != DateTime.DaysInMonth(date.m_dateTime.Year, date.m_dateTime.Month);
+        }
+
+        // перегрузка операторов true false
+        public static bool operator true(CustomDate date)
+        {
+            return date.m_dateTime.Day == 1 && date.m_dateTime.Month == 1 ? true : false;
+        }
+        public static bool operator false(CustomDate date)
+        {
+            return date.m_dateTime.Day == 1 && date.m_dateTime.Month == 1 ? true : false;
+        }
+        // перегрузка опреации &
+        public static bool operator &(CustomDate dateOne, CustomDate dateTwo)
+        {
+            return dateOne.m_dateTime.Equals(dateTwo.m_dateTime);
+        }
+        // перегрузка метода ToString()
+        public override string ToString()
+        {
+            return m_dateTime.ToString();
+        }
+        public static explicit operator string(CustomDate date)
+        {
+            return date.ToString();
+        }
+        public static explicit operator CustomDate(string date)
+        {
+            return new CustomDate(DateTime.Parse(date));
+        }
+        // метод для получения даты предыдущего дня
+        public DateTime GetPreviousDayDate()
+        {
+             return m_dateTime.AddDays(-1);
+        }
+        // метод для получения даты следующего дня
+        public DateTime GetNextDayDate()
+        {
+            return m_dateTime.AddDays(1);
+        }
+        // метод для получения количества дней до конца месяца
+        public int CountDaysToEndOfMonth()
+        {
+            return DateTime.DaysInMonth(m_dateTime.Year,m_dateTime.Month)-m_dateTime.Day;
+        }
+
+    }
+}
---
+++ exercise_12_graphics/exercise_12_graphics_task_1/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace exercise_12_graphics_task_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        CustomDate customDate;
+        CustomDate customDateSecond;
+        public MainWindow()
+        {
+            InitializeComponent();
+            customDate = new CustomDate();
+            customDateSecond = new CustomDate();
+            dateInput.Text = customDate.m_dateTime.ToString().Substring(0, 10);
+            dateInputSeond.Text = customDate.m_dateTime.ToString().Substring(0, 10);
+        }
+
+        private void dateInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            ProcessDate();
+        }
+        private void ProcessDate()
+        {
+            try
+            {
+                customDate.m_dateTime = DateTime.Parse(dateInput.Text);
+                customDateSecond.m_dateTime = DateTime.Parse(dateInputSeond.Text);
+
+                indecsatros.Content = $"The date above three day by now date: {customDate[3].ToString("dd/MM/yy")} \nThe date down five day by now date: {customDate[-5].ToString("dd/MM/yy")}";
+                unarOperator.Content = $"Is this day not the last day of month: {(!customDate ? "yes" : "no")}";
+                trueFalseOperator.Content = $"Is this date is year start: {(customDate? "yes" : "no")}";
+
+                operatorAmpersant.Content = $"Is dates equals: {(customDate & customDateSecond? "yes" : "no")}";
+
+                string strDate = (string)customDate;
+
+                invocationCastToStr.Content = $"Explicit date from customDate to string: {strDate}";
+                invocationCastToDate.Content = $"Explicit date from string to CustomDate: {(CustomDate)strDate}";
+            }
+            catch
+            {
+                indecsatros.Content = "waiting input";
+                unarOperator.Content = "waiting input";
+                trueFalseOperator.Content = "waiting input";
+                operatorAmpersant.Content = "waiting input";
+                invocationCastToDate.Content = "waiting input";
+                invocationCastToStr.Content = "waiting input";
+            }
+        }
+    }
+}
